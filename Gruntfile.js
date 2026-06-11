e2860f06d7b02b8ced3e32a3b5de82eea9829cdd
Marc Brooks <IDisposable@gmail.com>
2026-06-11 17:24:29 -0500

    Added version and time to the minified/uglified header

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,7 +16,7 @@ module.exports = function (grunt) {
         },
         uglify: {
             options: {
-                banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n',
+                banner: '/*! <%= pkg.name %> v<%= pkg.version %> <%= grunt.template.today("UTC:yyyy-mm-dd HH:MM:ss Z") %> */\n',
                 sourceMap: true,
             },
             dist: {
